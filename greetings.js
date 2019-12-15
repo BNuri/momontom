5bff047dbcf5352386cc6453a23e348a0ef4fbca
BNURI <luckmon05@naver.com>
2019-12-16 01:49:13 +0900

    first commit

---
+++ greetings.js
@@ -0,0 +1,44 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+  SHOW = "showing";
+
+function saveName(name) {
+  localStorage.setItem(USER_LS, name);
+}
+
+function handleEvent(event) {
+  event.preventDefault();
+  const currentName = input.value;
+  console.log(currentName);
+  paintGreeting(currentName);
+  saveName(currentName);
+}
+
+function askForName() {
+  form.classList.add(SHOW);
+  form.addEventListener("submit", handleEvent);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOW);
+  greeting.classList.add(SHOW);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
